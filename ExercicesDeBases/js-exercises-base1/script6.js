ea9f0756bea07025759d7bfcea1adce55d4dfc60
medleew <nadir.medlee@gmail.com>
2018-06-05 15:42:17 +0200

    Ajout de l'exo 6 avec fichier annexe script

---
+++ ExercicesDeBases/js-exercises-base1/script6.js
@@ -0,0 +1,5 @@
+function division(){
+    var nbre1 = Number.parseInt(prompt("Veuillez entrer un nombre réel"));
+    var nbre2 = Number.parseInt(prompt("Veuillez entrer un nombre réel"));
+    alert(nbre1%nbre2);
+}
