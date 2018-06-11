33413c3553f6eb96dcddfd382286efd56babd5ad
medleew <nadir.medlee@gmail.com>
2018-06-11 16:51:17 +0200

    Ajout exo 4 fini

---
+++ ExercicesDeBases/js-exercices-base3/exercice4/script.js
@@ -0,0 +1,11 @@
+var raz = document.getElementById('button');
+raz.addEventListener('click', function() {
+    document.getElementById('prenom').value = "";
+    document.getElementById('nom').value = "";
+    document.getElementById('ville').value = ""; 
+})
+
+
+
+
+
