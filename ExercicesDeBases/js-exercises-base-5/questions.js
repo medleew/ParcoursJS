145bcc4f4775385a01cdc76a5bae48304fc1a9d2
medleew <nadir.medlee@gmail.com>
2018-06-12 16:26:42 +0200

    ajout environnement de travail pour exo base 5 plus premier essaie de debug

---
+++ ExercicesDeBases/js-exercises-base-5/questions.js
@@ -0,0 +1,63 @@
+/**
+ * Exercice sur les chaines de caractères.
+ * Trouvez la façon de faire la plus optimal.
+ * Il peut y avoir plusieur façon de faire.
+ */
+var tailleString = function (texte) {
+    console.log("la taille de la chaine est de " + tailleString.length);
+}
+var remplaceECar = function (texte) {
+}
+var concatString = function (texte1, texte2) {
+
+}
+var afficherCar5 = function (texte) {
+
+}
+var afficher9Car = function (texte) {
+
+}
+var majusculeString = function (texte) {
+
+}
+var minusculeString = function (texte) {
+
+}
+var SupprEspaceString = function (texte) {
+
+}
+var IsString = function (texte) {
+
+}
+
+var AfficherExtensionString = function (texte) {
+
+}
+var NombreEspaceString = function (texte) {
+
+}
+var InverseString = function (texte) {
+
+}
+
+/**
+ * Exercices sur les nombres et les caluls mathématiques
+ */
+var calculPuissance = function (x, y) {
+
+}
+var valeurAbsolue = function (nombre) {
+
+}
+var valeurAbsolueArray = function (array) {
+
+}
+var sufaceCercle = function (rayon) {
+
+}
+var hypothenuse = function (ab, ac) {
+
+}
+var calculIMC = function (poids, taille) {
+
+}
