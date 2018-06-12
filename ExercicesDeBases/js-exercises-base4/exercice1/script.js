a883b500bde9da6e23b930222f364d8ca6b633b4
medleew <nadir.medlee@gmail.com>
2018-06-12 11:34:24 +0200

    Ajout exo 1 fini

---
+++ ExercicesDeBases/js-exercises-base4/exercice1/script.js
@@ -0,0 +1,10 @@
+let image = document.getElementById('image1');
+    image.addEventListener ('mouseover', function () { 
+    image.style = " border: 3px solid red";
+    })
+    image.addEventListener('mouseout', function () {
+    image.style = "borderStyle: none";
+    })
+
+
+
