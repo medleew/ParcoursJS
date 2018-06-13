cc1c5d994dfe707660b512a6dee1afbb6172eca4
medleew <nadir.medlee@gmail.com>
2018-06-13 10:55:46 +0200

    Ajout exo de base 5 avec tout les debugs

--- ExercicesDeBases/js-exercises-base-5/questions.js
+++ ExercicesDeBases/js-exercises-base-5/questions.js
@@ -4,60 +4,65 @@
  * Il peut y avoir plusieur façon de faire.
  */
 var tailleString = function (texte) {
-    console.log("la taille de la chaine est de " + tailleString.length);
+    return( texte.length);
 }
 var remplaceECar = function (texte) {
+    return 'C  texte à une certaine taille';
 }
 var concatString = function (texte1, texte2) {
+    return texte1 + texte2;
 
 }
 var afficherCar5 = function (texte) {
-
+    return texte.charAt(4)
 }
 var afficher9Car = function (texte) {
-
+    return texte.slice(0, 9);
 }
 var majusculeString = function (texte) {
-
+    return texte.toUpperCase()
 }
 var minusculeString = function (texte) {
-
+    return texte.toLowerCase()
 }
 var SupprEspaceString = function (texte) {
-
+    return texte.trim()
 }
 var IsString = function (texte) {
-
+    return typeof texte == "string";
 }
-
 var AfficherExtensionString = function (texte) {
-
+    return texte.split('.').pop(); 
 }
 var NombreEspaceString = function (texte) {
-
+    return (texte.split(" ").length - 1);
 }
 var InverseString = function (texte) {
-
+    return texte.split('').reverse().join("");
 }
+
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
 var calculPuissance = function (x, y) {
-
+    return Math.pow(x, y);
 }
 var valeurAbsolue = function (nombre) {
-
+    return Math.abs(nombre);
 }
 var valeurAbsolueArray = function (array) {
-
+    for (var i = 0; i < array.length; i++) {
+        array[i] = Math.abs(array[i]);
+      }
+      return array;
 }
 var sufaceCercle = function (rayon) {
-
+    return Math.round(Math.PI * (rayon * rayon));
 }
 var hypothenuse = function (ab, ac) {
-
+    return Math.hypot(ab, ac);
 }
 var calculIMC = function (poids, taille) {
-
+    return Math.round((poids / (taille * taille)) * 100) / 100;
 }
