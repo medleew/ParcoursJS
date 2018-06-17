93bb15b58870af86d8d0581aec109836918174a0
Medleew <nadir.medlee@gmail.com>
2018-06-17 21:23:14 +0200

    Ajout challenge semaine make a clock fini

---
+++ ExercicesDeBases/little_projects/JS_clock/script.js
@@ -0,0 +1,20 @@
+const secondHand = document.querySelector('.secondHand');
+const minHand = document.querySelector('.minHand');
+const hourHand = document.querySelector('.hourHand');
+// let utcDate = new Date (Date.UTC(2018,6,17,21,10));
+// let utcDate = new Date (Date.UTC);
+function setDate () {
+    // const now = utcDate; 
+    const now = new Date ();
+    const seconds = now.getSeconds();
+    const secondsDegrees = (( seconds/60 ) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const minuts = now.getMinutes();
+    const minutsDegrees = ((minuts/60) * 360) + 90;
+    minHand.style.transform = `rotate(${minutsDegrees}deg)`;
+    const hours = now.getHours();
+    const hoursDegrees = ((hours/60) * 360 ) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval (setDate,1000);
