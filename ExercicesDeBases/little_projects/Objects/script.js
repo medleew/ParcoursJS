35dd6e80b46aef61c0b3fd67c397588042253d02
medleew <nadir.medlee@gmail.com>
2018-06-19 18:05:09 +0200

    supprimer la boucle for

--- ExercicesDeBases/little_projects/Objects/script.js
+++ ExercicesDeBases/little_projects/Objects/script.js
@@ -11,22 +11,24 @@ for (const prop in Character) {
   }
   
 function giveItem () {
-    for (let i = 0; i < Character.itemToGive.length; i++) {
-        let randomObject = Character.itemToGive[Math.floor(Math.random() *
-            Character.itemToGive.length)];
-            if (randomObject == 'stick') {
-                alert('The ' + PNJ + ' gave you a ' + randomObject );
-            } else if (randomObject == 'water') {
-                alert('The ' + PNJ + ' gave you some ' + randomObject );
-            } else if (randomObject == 'rock') {
-                alert('The ' + PNJ + ' gave you a ' + randomObject );
+    let randomObject = Character.itemToGive[Math.floor(Math.random() *
+        Character.itemToGive.length)];
+        if (randomObject == 'stick') {
+            alert('The ' + PNJ + ' gave you a ' + randomObject );
+        } else if (randomObject == 'water') {
+            alert('The ' + PNJ + ' gave you some ' + randomObject );
+        } else if (randomObject == 'rock') {
+            alert('The ' + PNJ + ' gave you a ' + randomObject );
 
-            } else if (randomObject == 'fire') {
-                alert('The ' + PNJ + ' gave you some ' + randomObject );
-            }
-            break;
-    }
+        } else if (randomObject == 'fire') {
+            alert('The ' + PNJ + ' gave you some ' + randomObject );
+        }
+
+        // ajout switch dans 6 mois
 }
 
 giveItem();
+
+// let objects = [['épée', 'Masamune', 20, 5 , 2, true], [], []];
+
 
