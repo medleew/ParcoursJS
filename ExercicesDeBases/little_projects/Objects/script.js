5c7ad6142d83279822c42220044c08701c17d2ba
medleew <nadir.medlee@gmail.com>
2018-06-20 12:06:58 +0200

    mettre les deux première partie (PNJ + Shop) en commantaire

--- ExercicesDeBases/little_projects/Objects/script.js
+++ ExercicesDeBases/little_projects/Objects/script.js
@@ -7,7 +7,7 @@
 // };
 
 // for (const prop in Character) {
-//     console.log(`Character.${prop} = ${Character[prop]}`);
+//     console.log(`${prop} = ${Character[prop]}`);
 //   }
   
 // function giveItem () {
@@ -29,38 +29,38 @@
 
 //  giveItem();
 
-let objects = [ {Description: 'Sword', Name: 'Masamune', Physic: 45, Magic: 5, minlvl:5, Available: true },
-                {Description: 'Axe', Name:'Leviathan', Physic:50, Magic:15, minlvl:20, Available:false},   
-                {Description: 'Staff', Name:'Benediction', Physic:5, Magic:40, minlvl:20, Available:true},
-    ];
-    // console.log(objects[0].Description); ---> pour afficher premier object à la première position 
-    for (let object of objects) {
-        console.log(object.Description); // plus direct et sûr
-    }
-    // for (let i = 0; i< 3; i++) {
-    //     let object = objects[i].Description; // bon mais il y a plus simple et plus rapide ( plus haut )
-    //     console.log(object);
-    // }
+// let objects = [ {Description: 'Sword', Name: 'Masamune', Physic: 45, Magic: 5, minlvl:5, Available: true },
+//                 {Description: 'Axe', Name:'Leviathan', Physic:50, Magic:15, minlvl:20, Available:false},   
+//                 {Description: 'Staff', Name:'Benediction', Physic:5, Magic:40, minlvl:20, Available:true},
+//     ];
+//     // console.log(objects[0].Description); ---> pour afficher premier object à la première position 
+//     for (let object of objects) {
+//         console.log(object.Description); // plus direct et sûr
+//     }
+//     // for (let i = 0; i< 3; i++) {
+//     //     let object = objects[i].Description; // bon mais il y a plus simple et plus rapide ( plus haut )
+//     //     console.log(object);
+//     // }
 
-    function objectAvailable () {
-        for (let object of objects) {
-            if (object.Available == true) {
-                console.log("The " + object.Description + " is available");                 
-            } else {
-                console.log("The " + object.Description + " is not available");
-            }
-        }
-    }
-    objectAvailable();
+//     function objectAvailable () {
+//         for (let object of objects) {
+//             if (object.Available == true) {
+//                 console.log("The " + object.Description + " is available");                 
+//             } else {
+//                 console.log("The " + object.Description + " is not available");
+//             }
+//         }
+//     }
+//     objectAvailable();
 
-    function stuffLvlMin () {
-        for (let object of objects) {   
-            if (object.minlvl > 10) {
-                console.log("You have the lvl required to have " + object.Description);
-            } else {
-                console.log("This " + object.Description + " is not available for your lvl");
-            }
-        }    
-    }
-    stuffLvlMin();
+//     function stuffLvlMin () {
+//         for (let object of objects) {   
+//             if (object.minlvl > 10) {
+//                 console.log("You have the lvl required to have " + object.Description);
+//             } else {
+//                 console.log("This " + object.Description + " is not available for your lvl");
+//             }
+//         }    
+//     }
+//     stuffLvlMin();
 
